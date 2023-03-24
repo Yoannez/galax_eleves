3d6b0858c070b3ec3422b8566d6a908cd071346c
Qichao HU <q21hu@pc-elec-385.priv.enst-bretagne.fr>
2023-03-24 11:54:21 +0100

    Update GPU

--- src/Model/Model_GPU/kernel.cu
+++ src/Model/Model_GPU/kernel.cu
@@ -7,13 +7,45 @@
 
 __global__ void compute_acc(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU, float* massesGPU, int n_particles)
 {
-	// unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	if(i < n_particles){
+		for(int j = 0; j < n_particles; j++){
+			if(i != j)
+			{
+				const float diffx = positionsGPU[j].x - positionsGPU[i].x;
+				const float diffy = positionsGPU[j].y - positionsGPU[i].y;
+				const float diffz = positionsGPU[j].z - positionsGPU[i].z;
+				
+				float dij = diffx * diffx + diffy * diffy + diffz * diffz;
+
+				if (dij < EPS)
+				{
+					dij = 10.0;
+				}
+				else
+				{
+					dij = rsqrtf(dij);
+					dij = 10.0 * (dij * dij * dij);
+				}
+				accelerationsGPU[i].x += diffx * dij * massesGPU[j];
+				accelerationsGPU[i].y += diffy * dij * massesGPU[j];
+				accelerationsGPU[i].z += diffz * dij * massesGPU[j];
+			}
+		}
+	}
+	
 }
 
 __global__ void maj_pos(float3 * positionsGPU, float3 * velocitiesGPU, float3 * accelerationsGPU)
 {
-	// unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+	velocitiesGPU[i].x += accelerationsGPU[i].x * 2.0f;
+	velocitiesGPU[i].y += accelerationsGPU[i].y * 2.0f;
+	velocitiesGPU[i].z += accelerationsGPU[i].z * 2.0f;
 
+	positionsGPU[i].x += velocitiesGPU[i].x * DIFF_T ;
+	positionsGPU[i].y += velocitiesGPU[i].y * DIFF_T ;
+	positionsGPU[i].z += velocitiesGPU[i].z * DIFF_T ;
 }
 
 void update_position_cu(float3* positionsGPU, float3* velocitiesGPU, float3* accelerationsGPU, float* massesGPU, int n_particles)
